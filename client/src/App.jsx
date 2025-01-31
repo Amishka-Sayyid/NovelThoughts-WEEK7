a86214b83e7fff54f52919c23468d573a56971cc
Amina Said Ibrahim <amina7585@gmail.com>
2025-01-31 11:46:45 +0000

    initial commit with walking skeleton from class

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+//The App component is the parent of your other components
+
+//Here you should set up your routes
+export default function App() {
+  return (
+    <>
+      <h1>App</h1>
+      {/* Routes go here! */}
+    </>
+  );
+}
