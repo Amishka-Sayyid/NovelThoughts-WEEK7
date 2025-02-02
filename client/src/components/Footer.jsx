8aa4eb69e9a5e1c97d38648c1c4472666a4bfedd
Amina Said Ibrahim <amina7585@gmail.com>
2025-02-02 15:03:32 +0000

    basic styling and color pallete choice

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,6 +3,14 @@ export default function Footer() {
   return (
     <>
       <footer className="footer">
+        <div className="sociallinks">
+          <a href="https://github.com/Amishka-Sayyid" target="_blank">
+            <i className="fa-brands fa-github"></i>
+          </a>
+          <a href="" target="_blank">
+            <i className="fa-brands fa-linkedin"></i>
+          </a>
+        </div>
         <h1> © 2025 Novel Attic</h1>
       </footer>
     </>
--- client/src/components/Section.jsx
+++ client/src/components/Section.jsx
@@ -6,27 +6,29 @@ import Contact from "./Contact";
 export default function Section() {
   return (
     <>
-      <section>
-        <div>
-          <h1>Welcome to Novel Attic</h1>
-          <h4>Where Books Meet Conversation</h4>
+      <section className="center">
+        <div className="MainSection">
+          <div>
+            <h1>Welcome to Novel Attic</h1>
+            <h4>Where Books Meet Conversation</h4>
+          </div>
+          {/* Navigation Links */}
+          <nav>
+            <Link to="/">
+              <button>About</button>
+            </Link>
+
+            <Link to="/book">
+              <button>Books</button>
+            </Link>
+
+            <Link to="/contact">
+              <button>Contact</button>
+            </Link>
+          </nav>
         </div>
-        {/* Navigation Links */}
-        <nav>
-          <Link to="/">
-            <button>About</button>
-          </Link>
-
-          <Link to="/book">
-            <button>Books</button>
-          </Link>
-
-          <Link to="/contact">
-            <button>Contact</button>
-          </Link>
-        </nav>
         {/* dynamic content area */}
-        <div className="contentSection">
+        <div className="linkViewSection">
           <Routes>
             <Route path="/" element={<About />} />
             <Route path="/book" element={<Books />} />
