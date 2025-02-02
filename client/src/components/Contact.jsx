3994773b3962c9754ac064f80429f8c8b488935d
Amina Said Ibrahim <amina7585@gmail.com>
2025-02-02 19:58:41 +0000

    done contact part

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,9 +1,39 @@
+import "./About.css";
+
+import { Link } from "react-router-dom";
+
 export default function Contact() {
   return (
     <>
-      <div>
-        <h1>contact part</h1>
-      </div>
+      <section className="About">
+        <div className="AboutImg">
+          <img
+            src="https://media.istockphoto.com/id/949118068/photo/books.jpg?s=612x612&w=0&k=20&c=1vbRHaA_aOl9tLIy6P2UANqQ27KQ_gSF-BH0sUjQ730="
+            alt="A stack of books on a table"
+          />
+        </div>
+        <div className="AboutText">
+          <h2>Contact Us</h2>
+          <ul>
+            <ol>Phone Number:+44 7234567891 </ol>
+            <ol>Email: novelattic@gmail.com</ol>
+            <ol>Address: 12 anywhere street, le1 2mk</ol>
+          </ul>
+          <div>
+            <Link to="/">
+              <button>About</button>
+            </Link>
+
+            <Link to="/book">
+              <button>Books</button>
+            </Link>
+
+            <Link to="/contact">
+              <button>Contact</button>
+            </Link>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
