95ad3e675bef16bb09eb2abd725b63664ef53c3e
Amina Said Ibrahim <amina7585@gmail.com>
2025-02-03 01:11:25 +0000

    added a go to  top button

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,6 +3,9 @@ import "./media.css";
 export default function Footer() {
   return (
     <>
+      <a className="gotopbtn" href="">
+        <i className="fa-solid fa-arrow-up"></i>
+      </a>
       <footer className="footer">
         <div className="sociallinks">
           <a href="https://github.com/Amishka-Sayyid" target="_blank">
